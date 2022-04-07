274a90095f69844e186da9a23eb56dabb35a6aa5
AleksandrFR <79194849399@yandex.ru>
2022-04-07 21:59:10 +0300

    update commit - add parseXML

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,26 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    private static List parseCSV(String[] columnMapping, String fileName) {
+
+    private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
         try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
             ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
             strategy.setType(Employee.class);
@@ -21,8 +32,7 @@ public class Main {
             CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
                     .withMappingStrategy(strategy)
                     .build();
-            List<Employee> list = csv.parse();
-            return list;
+            return csv.parse();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -34,12 +44,11 @@ public class Main {
         Gson gson = builder.create();
         Type listType = new TypeToken<List<Employee>>() {
         }.getType();
-        String json = gson.toJson(list, listType);
-        return json;
+        return gson.toJson(list, listType);
     }
 
-    private static void writeString(String json) {
-        try (FileWriter writer = new FileWriter("C:\\Users\\sasha\\IdeaProjects\\specialfilestask1\\data.json", false)) {
+    private static void writeString(String json, String fileNumber) {
+        try (FileWriter writer = new FileWriter("C:\\Users\\sasha\\IdeaProjects\\specialfilestask1\\data" + fileNumber + ".json", false)) {
             writer.write(json);
         } catch (IOException ex) {
             System.out.println(ex.getMessage(
@@ -50,9 +59,43 @@ public class Main {
     public static void main(String[] args) {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
-        List<Employee> list = parseCSV(columnMapping, fileName);
-        String json = listToJson(list);
-        writeString(json);
+        List<Employee> list1 = parseCSV(columnMapping, fileName);
+        writeString(listToJson(list1), "1");
+        List<Employee> list2 = parseXML("data.xml");
+        writeString(listToJson(list2), "2");
+
+    }
+
+    private static List<Employee> parseXML(String s) {
+        List<Employee> staff = new ArrayList<>();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new File(s));
+            Node root = document.getDocumentElement();
+            NodeList nodeList = root.getChildNodes();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (Node.ELEMENT_NODE == node.getNodeType()) {
+                    Element element = (Element) node;
+                    int id = Integer.parseInt(element.getElementsByTagName("id").item(0).getTextContent());
+                    String firstName = element.getElementsByTagName("firstName").item(0).getTextContent();
+                    String lastName = element.getElementsByTagName("lastName").item(0).getTextContent();
+                    String country = element.getElementsByTagName("country").item(0).getTextContent();
+                    int age = Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent());
+                    Employee employee = new Employee(id, firstName, lastName, country, age);
+                    staff.add(employee);
+                }
+            }
+            return staff;
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
 
